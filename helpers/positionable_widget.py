f026370e8df2c0204035a3d525621e4253645d6a
koztay <koztay@me.com>
2018-07-13 23:03:36 +0300

    programmatik olarak drawing area resim ekleyebildim

--- helpers/positionable_widget.py
+++ helpers/positionable_widget.py
@@ -32,12 +32,24 @@ class MainWindow(Gtk.ApplicationWindow):
         # Add the button in the (x=20,y=100) position
         container.put(another_button, 400, 100)  # ekranın dışına taşıramadığı için butonun yarısını gösteriyor.
 
-        # create an image
+        # # create an image
+        # image = Gtk.Image()
+        # # set the content of the image as the file filename.png
+        # image.set_from_file("/Users/kemal/WorkSpace/Videowall Development/media/tvlogo_full.png")
+        # # add the image to the window
+        # container.put(image, 1050, 50)
+
+
+    def add_image(self, container):
         image = Gtk.Image()
         # set the content of the image as the file filename.png
         image.set_from_file("/Users/kemal/WorkSpace/Videowall Development/media/tvlogo_full.png")
         # add the image to the window
         container.put(image, 1050, 50)
+
+    def add_drawing_area(self, container):
+        videowidget = Gtk.DrawingArea()
+        container.put(videowidget, 300, 300)
 
 
 class Application(Gtk.Application):
@@ -47,6 +59,12 @@ class Application(Gtk.Application):
 
     def do_activate(self):
         self.mainWindow = MainWindow(self)
+        # print(dir(self.mainWindow))
+        # get object of fixed widget
+        fixed_widget = self.mainWindow.get_child()
+        # run function to add image
+        self.mainWindow.add_image(fixed_widget)
+        self.mainWindow.add_drawing_area(fixed_widget)
         self.mainWindow.show_all()
 
     def do_startup(self):
