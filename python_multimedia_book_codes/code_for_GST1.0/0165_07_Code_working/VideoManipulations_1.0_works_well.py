545d297178c453f2d4927803857b5e590f6ba72b
koztay <koztay@me.com>
2018-07-27 16:58:32 +0300

    encoding declaration eklendi

--- python_multimedia_book_codes/code_for_GST1.0/0165_07_Code_working/VideoManipulations_1.0_works_well.py
+++ python_multimedia_book_codes/code_for_GST1.0/0165_07_Code_working/VideoManipulations_1.0_works_well.py
@@ -257,6 +257,9 @@ class VideoPlayer:
 # Run the program
 
 Gst.init(None)
+Gst.debug_set_colored(Gst.DebugColorMode.ON)
+Gst.debug_set_active(True)
+Gst.debug_set_default_threshold(4)
 player = VideoPlayer()
 thread = threading.Thread(target=player.play)
 thread.start()
