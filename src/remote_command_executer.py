9ac43af7e806a064adb8c47f7155f035fa3eccf8
koztay <koztay@me.com>
2018-07-30 16:03:30 +0300

    playbin_player 'ı refactoring yapıyorum

--- src/remote_command_executer.py
+++ src/remote_command_executer.py
@@ -3,7 +3,7 @@ import Pyro4
 import time
 
 gi.require_version('Gtk', '3.0')
-gi.require_version('Gtk', '1.0')
+gi.require_version('Gst', '1.0')
 from gi.repository import Gtk, Gdk, Gst
 from playbin_player import VideoPlayer as playbin_player
 
