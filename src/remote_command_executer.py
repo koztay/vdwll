349efe44420f2ca07ad26261bc1e2e1095c070d2
koztay <koztay@me.com>
2018-07-31 15:22:52 +0300

    resize etmediğimiz için ekle çıkar yapmadan sadece crop problemsiz çalıştı.

--- src/remote_command_executer.py
+++ src/remote_command_executer.py
@@ -86,13 +86,6 @@ class RemoteCommander(object):
         videowidget.set_size_request(width, height)
         videowidget.show()
         videowidget.player = playbin_player(uri=uri, moviewindow=videowidget, width=width, height=height)
-        # videowidget.player.caps = Gst.Caps.from_string("video/x-raw, width={}, height={}".format(
-        #     width, height
-        # ))
-        # videowidget.player.videobox.set_property("bottom", crop_bottom)
-        # videowidget.player.videobox.set_property("top", crop_top)
-        # videowidget.player.videobox.set_property("left", crop_left)
-        # videowidget.player.videobox.set_property("right", crop_right)
         container.put(videowidget, xpos, ypos)
 
     def change_mod_queue(self,
@@ -110,87 +103,8 @@ class RemoteCommander(object):
             print("name :", child.get_name())
             if child.get_name() == name:
                 print("bakalım player var mı?", child.player)
-
-                # child.player.caps = Gst.Caps.from_string("video/x-raw, width={}, height={}".format(
-                #     width, height
-                # ))
-                # child.player.filter = Gst.ElementFactory.make("capsfilter", "filter")
-
-                # if width and height:
-                #     # child.player.filter.set_state(Gst.State.NULL)
-                #     child.player.caps = Gst.Caps.from_string("video/x-raw, width={}, height={}".format(
-                #         width, height
-                #     ))
-                #     child.player.filter = Gst.ElementFactory.make("capsfilter", "filter")
-                # elif width:
-                #     child.player.caps = Gst.Caps.from_string("video/x-raw, width={}".format(
-                #         width
-                #     ))
-                #     child.player.filter = Gst.ElementFactory.make("capsfilter", "filter")
-                # elif height:
-                #     child.player.caps = Gst.Caps.from_string("video/x-raw, height={}".format(
-                #         height
-                #     ))
-                #
-                # # GObject.timeout_add(100, child.player.videobox.set_property("bottom", crop_bottom))
-                # # GObject.timeout_add(200, child.player.videobox.set_property("top", crop_top))
-                # # GObject.timeout_add(300, child.player.videobox.set_property("left", crop_left))
-                # # GObject.timeout_add(400, child.player.videobox.set_property("right", crop_right))
                 #
                 child.player.videobox.set_property("bottom", crop_bottom)
                 child.player.videobox.set_property("top", crop_top)
                 child.player.videobox.set_property("left", crop_left)
                 child.player.videobox.set_property("right", crop_right)
-                #
-                # child.player.timeoverlay.set_property("text",
-                #                                       "w:{}, h:{}, left:{},top:{}, right:{}, bottom:{}".format(
-                #                                           width, height, crop_left, crop_top, crop_right,
-                #                                           crop_bottom))
-
-                # start = time.time()
-                # status_nulled = child.player.data.pipeline.set_state(Gst.State.NULL)
-                # print("status_paused pipeline", status_nulled)
-                #
-                # # child.player.bin.set_state(Gst.State.NULL) yukarıdaki kod bunu da NULL yaptı zaten
-                # status_removed = child.player.bin.remove(child.player.queue)  # burada çıkarttım
-                # print("status_removed mod queue", status_removed)
-                #
-                # # burada yenisini ekledim
-                # child.player.construct_mod_queue(
-                #     video_width=width,
-                #     video_height=height,
-                #     crop_left=crop_left,
-                #     crop_right=crop_right,
-                #     crop_bottom=crop_bottom,
-                #     crop_top=crop_top)
-                #
-                # # # burada da başlattım
-                # # status_playing = child.player.bin.set_state(Gst.State.PLAYING)
-                # # print("status_playing mod queue", status_playing)
-                #
-                # status_playing_pipe = child.player.data.pipeline.set_state(Gst.State.PLAYING)
-                # print("status_playing mod queue", status_playing_pipe)
-                # finish = time.time()
-                # duration = finish - start
-                # print("duration :", duration)  # bu yaklaşık 50 ms sürüyor. Ama playe başlaması uzun sürüyor.
-                # belki lokal networkdeki source 'lar için uzun sürmez bu...
-
-                """
-                https://github.com/sdroege/gst-snippets/blob/217ae015aaddfe3f7aa66ffc936ce93401fca04e/dynamic-filter.c
-                aşağıdaki kodda pipeline 'ı durdurmamış... Acaba otomatik mi duruyor? Dene...
-                gst_bin_remove (GST_BIN (pipeline), filter);
-                gst_bin_remove (GST_BIN (pipeline), conv2);
-            
-                gst_element_set_state (filter, GST_STATE_NULL);
-                gst_element_set_state (conv2, GST_STATE_NULL);
-            
-                gst_object_unref (filter);
-                gst_object_unref (conv2);
-                filter = NULL;
-                conv2 = NULL;
-                """
-
-                """
-                Burada da videosink için add remove kodu var C olarak :
-                https://github.com/sdroege/gst-snippets/blob/217ae015aaddfe3f7aa66ffc936ce93401fca04e/dynamic-tee-vsink.c
-                """
